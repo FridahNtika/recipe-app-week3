9a5663b8ec9a31b472946b5ee2190553d0e7eb43
Sara Inoue <sarainoue11@gmail.com>
2024-06-04 11:52:19 -0800

    set up the connection to get the backend information in the firebase database.

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 
 const express = require("express");
+const recipeRouter = require('./recipes');
+
 const app = express();
 const port = 5001;
 app.use(express.json());
+
+
 const db = require("./firebase");
 const { collection, getDocs, updateDoc, doc, addDoc, deleteDoc } = require("firebase/firestore");
 
 const cors = require("cors");
 app.use(cors());
+
 app.get('/test', async (req, res) => {
     try {
         const snapshot = await getDocs(collection(db, 'test'));
@@ -20,6 +25,11 @@ app.get('/test', async (req, res) => {
         res.status(500).send({ error: 'Error fetching messages' });
     }
 });
+
+app.use("/recipes", recipeRouter);
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
--- frontend/recipes/src/roots/Recipes.jsx
+++ frontend/recipes/src/roots/Recipes.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import '../styles/recipes.css';
+import axios from 'axios';
+
 
 const Recipe = () => {
+  const [loading, setLoading] = useState(true);
+  const [recipeArray, setRecipeArray] = useState([]);
+  const [currentRecipe, setCurrentRecipe] = useState('');
+
+async function fetchAllRecipes(){
+  try{
+    const res = await axios.get("http://localhost:5001/recipes");
+    setRecipeArray(res.data);
+    setLoading(false);
+  } catch (error) {
+    console.error("Error fetching Recipes: ", error);
+    setLoading(false);
+  }
+}
+
+
+  useEffect(() => {
+    fetchAllRecipes();
+  })
+
+  
+
   return (
     <div>
       <h1>Recipes Page</h1>
