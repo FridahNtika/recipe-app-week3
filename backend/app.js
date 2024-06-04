e5805c63066d363579d2b5b02d40dcd450efd5be
Sara Inoue <sarainoue11@gmail.com>
2024-06-04 07:17:29 -0800

    added firebase base, should be able to localhost:5001/test to fetch mock data

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+const app = express();
+const port = 5001;
+app.use(express.json());
+const db = require("./firebase");
+const { collection, getDocs, updateDoc, doc, addDoc, deleteDoc } = require("firebase/firestore");
+
+const cors = require("cors");
+app.use(cors());
+app.get('/test', async (req, res) => {
+    try {
+        const snapshot = await getDocs(collection(db, 'test'));
+        const messages = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        res.status(200).send(messages);
+    } catch (error) {
+        res.status(500).send({ error: 'Error fetching messages' });
+    }
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
