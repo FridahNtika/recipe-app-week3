43efd2fe889a15d8c22d5a8627bf0a7fe302291e
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-06-04 15:10:21 -0400

    initial commit for MyRecipes page

--- frontend/recipes/src/roots/MyRecipes.jsx
+++ frontend/recipes/src/roots/MyRecipes.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import {
+  Container,
+  Card,
+  Box,
+  Text,
+  Spinner,
+  VStack,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  HStack,
+  Flex,
+  IconButton,
+  useToast
+} from '@chakra-ui/react';
 
 const MyRecipes = () => {
   return (
-    <div>
+    <>
       <h1>My Recipe Page</h1>
-    </div>
+      <VStack spacing={4} align="stretch">
+      <Box 
+          p={6} 
+          bg="blue.500" 
+          borderRadius="md" 
+          width="100%" 
+          _hover={{ boxShadow: 'dark-lg' }}>
+          <Text color="white">Test Card</Text>
+        </Box>
+        <Box 
+          p={6} 
+          bg="blue.500" 
+          borderRadius="md" 
+          width="100%" 
+          _hover={{ boxShadow: 'dark-lg' }}>
+          <Text color="white">Test Card</Text>
+        </Box>
+      </VStack>
+    </>
   )
 }
 
