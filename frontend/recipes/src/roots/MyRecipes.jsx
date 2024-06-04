e203174bf2ff11246f7eec94513ca0fe90a36501
Anh Tuong Nguyen <aanhnguyen2023@gmail.com>
2024-06-04 13:06:21 -0700

    fixed margins for navbar

--- frontend/recipes/src/roots/MyRecipes.jsx
+++ frontend/recipes/src/roots/MyRecipes.jsx
@@ -1,46 +1,10 @@
 import React from 'react'
-import NavBar from '../components/NavBar';
-import {
-  Container,
-  Card,
-  Box,
-  Text,
-  Spinner,
-  VStack,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Textarea,
-  HStack,
-  Flex,
-  IconButton,
-  useToast
-} from '@chakra-ui/react';
 
 const MyRecipes = () => {
   return (
     <div>
       <h1>My Recipe Page</h1>
-      <VStack spacing={4} align="stretch">
-      <Box 
-          p={6} 
-          bg="blue.500" 
-          borderRadius="md" 
-          width="100%" 
-          _hover={{ boxShadow: 'dark-lg' }}>
-          <Text color="white">Test Card</Text>
-        </Box>
-        <Box 
-          p={6} 
-          bg="blue.500" 
-          borderRadius="md" 
-          width="100%" 
-          _hover={{ boxShadow: 'dark-lg' }}>
-          <Text color="white">Test Card</Text>
-        </Box>
-      </VStack>
-    </>
+    </div>
   )
 }
 
