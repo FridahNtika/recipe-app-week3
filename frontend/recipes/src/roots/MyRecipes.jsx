95b32278afc06d0d35b79054750d65ad5cf84a05
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-06-04 15:35:38 -0400

    Add box wrapping

--- frontend/recipes/src/roots/MyRecipes.jsx
+++ frontend/recipes/src/roots/MyRecipes.jsx
@@ -1,30 +1,59 @@
 import React from 'react'
-import { VStack, Box, Text } from '@chakra-ui/react';
+import {
+  Container,
+  Box,
+  Text,
+  Wrap, 
+  WrapItem 
+} from '@chakra-ui/react';
 
 const MyRecipes = () => {
   return (
-    
-    <div>
-      <h1>My Recipe Page</h1>
-      <VStack spacing={4} align="stretch">
-      <Box 
-          p={6} 
-          bg="blue.500" 
-          borderRadius="md" 
-          width="100%" 
-          _hover={{ boxShadow: 'dark-lg' }}>
-          <Text color="white">Test Card</Text>
-        </Box>
-        <Box 
-          p={6} 
-          bg="blue.500" 
-          borderRadius="md" 
-          width="100%" 
-          _hover={{ boxShadow: 'dark-lg' }}>
-          <Text color="white">Test Card</Text>
-        </Box>
-      </VStack>
-    </div>
+    <Container>
+      <Text fontSize="2xl" mb={4}>My Recipe Page</Text>
+      <Wrap spacing="30px">
+        <WrapItem>
+          <Box 
+            p={6} 
+            bg="blue.500" 
+            borderRadius="md" 
+            width="200px" 
+            _hover={{ boxShadow: 'dark-lg' }}>
+            <Text color="white">Test Card 1</Text>
+          </Box>
+        </WrapItem>
+        <WrapItem>
+          <Box 
+            p={6} 
+            bg="blue.500" 
+            borderRadius="md" 
+            width="200px" 
+            _hover={{ boxShadow: 'dark-lg' }}>
+            <Text color="white">Test Card 2</Text>
+          </Box>
+        </WrapItem>
+        <WrapItem>
+          <Box 
+            p={6} 
+            bg="blue.500" 
+            borderRadius="md" 
+            width="200px" 
+            _hover={{ boxShadow: 'dark-lg' }}>
+            <Text color="white">Test Card 3</Text>
+          </Box>
+        </WrapItem>
+        <WrapItem>
+          <Box 
+            p={6} 
+            bg="blue.500" 
+            borderRadius="md" 
+            width="200px" 
+            _hover={{ boxShadow: 'dark-lg' }}>
+            <Text color="white">Test Card 4</Text>
+          </Box>
+        </WrapItem>
+      </Wrap>
+    </Container>
   )
 }
 
