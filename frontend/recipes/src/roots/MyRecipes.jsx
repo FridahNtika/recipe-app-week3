fc79c56cba8f0a141b7b2b941c949443a0a28924
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-06-05 11:14:07 -0400

    add box styling

--- frontend/recipes/src/roots/MyRecipes.jsx
+++ frontend/recipes/src/roots/MyRecipes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Box, Text, Wrap, WrapItem, Center } from '@chakra-ui/react';
+import { Container, Box, Text, Wrap, WrapItem, Center, Button, Flex } from '@chakra-ui/react';
 
 const MyRecipes = () => {
   const [recipes, setRecipes] = useState([]);
@@ -22,50 +22,28 @@ const MyRecipes = () => {
 
   return (
     <>
-    <Text fontSize="2xl" mb={4}>My Recipes</Text>
+      <Text fontSize="4xl" mb={4}>My Recipes</Text>
       <Wrap spacing="30px">
         {recipes.map(recipe => (
           <WrapItem key={recipe.id}>
             <Box
               p={6}
-              bg="blue.500"
+              bg="#90B4CE"
               borderRadius="md"
               width="20vw"
-              height="35vh"
-              _hover={{ boxShadow: 'dark-lg' }}>
+              height="40vh"
+              _hover={{ boxShadow: 'dark-lg' }}
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Text>Picture goes here</Text>
               <Center>
-              <Text color="white" alignContent={"center"}>{recipe.name}</Text>
+                <Text fontSize={24}>{recipe.name}</Text>
               </Center>
-            </Box>
-          </WrapItem>
-        ))}
-          {recipes.map(recipe => (
-          <WrapItem key={recipe.id}>
-            <Box
-              p={6}
-              bg="blue.500"
-              borderRadius="md"
-              width="20vw"
-              height="35vh"
-              _hover={{ boxShadow: 'dark-lg' }}>
-              <Center>
-              <Text color="white" alignContent={"center"}>{recipe.name}</Text>
-              </Center>
-            </Box>
-          </WrapItem>
-        ))}
-        {recipes.map(recipe => (
-          <WrapItem key={recipe.id}>
-            <Box
-              p={6}
-              bg="blue.500"
-              borderRadius="md"
-              width="20vw"
-              height="35vh"
-              _hover={{ boxShadow: 'dark-lg' }}>
-              <Center>
-              <Text color="white" alignContent={"center"}>{recipe.name}</Text>
-              </Center>
+              <Flex justifyContent="flex-end" mt={4}>
+                <Button>Save</Button>
+              </Flex>
             </Box>
           </WrapItem>
         ))}
