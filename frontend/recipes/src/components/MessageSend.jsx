bef5ba35ac7021569908d82b739c2145fba67536
Vento24 <mvento@kountable.com>
2024-06-06 14:16:08 -0400

    removed unnecessary comments

--- frontend/recipes/src/components/MessageSend.jsx
+++ frontend/recipes/src/components/MessageSend.jsx
@@ -24,21 +24,6 @@ const MessageSend = ({ sendMessage }) => {
 
   return (
     <div>
-      {/* <InputGroup className="form-container">
-        <Form.Control
-          className="send-message"
-          placeholder="Type a message..."
-          value={message}
-          size="lg"
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={(e) => handleKeyPress(e)}
-          aria-label="Send message"
-          aria-describedby="basic-addon2"
-        />
-        <Button onClick={handleSubmit} className="send-button">
-          <img className="send-icon" src={sendIcon} alt="send icon" />
-        </Button>
-      </InputGroup> */}
       <FormControl isRequired className="form-container">
         <Input
           value={message}
